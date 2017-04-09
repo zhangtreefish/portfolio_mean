3b669ddc6e8523b4f4a38a7d05208509c0600ec6
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-04-09 13:59:02 -0500

    Fix: the [:nomod] error by adding myApp in test.js

--- portfolio/test.js
+++ portfolio/test.js
@@ -1,40 +1,40 @@
-// describe('Nav Bar', function() {
-//   var injector;
-//   var element;
-//   var scope;
-//   var intercepts;
-//   var httpBackend;
+describe('Nav Bar', function() {
+  var injector;
+  var element;
+  var scope;
+  var intercepts;
+  var httpBackend;
 
-//   beforeEach(function() {
-//     injector = angular.injector(['myApp.components', 'ngMockE2E']);
-//     intercepts = {};
+  beforeEach(function() {
+    injector = angular.injector(['myApp.components', 'ngMockE2E']);
+    intercepts = {};
 
-//     injector.invoke(function($rootScope, $compile, $httpBackend) {
-//       scope = $rootScope.$new();
+    injector.invoke(function($rootScope, $compile, $httpBackend) {
+      scope = $rootScope.$new();
 
-//       $httpBackend.whenGET(/.*\/templates\/.*/i).passThrough();
-//       httpBackend = $httpBackend;
+      $httpBackend.whenGET(/.*\/templates\/.*/i).passThrough();
+      httpBackend = $httpBackend;
 
-//       element = $compile('<nav-bar></nav-bar>')(scope);
-//       scope.$apply();
-//     });
-//   });
+      element = $compile('<nav-bar></nav-bar>')(scope);
+      scope.$apply();
+    });
+  });
 
-//   it('shows logged in users profile picture', function(done) {
-//     httpBackend.expectGET('/api/v1/me').respond({
-//       user: { profile: { picture: 'myPic' } }
-//     });
+  it('shows logged in users profile picture', function(done) {
+    httpBackend.expectGET('/api/v1/me').respond({
+      user: { profile: { picture: 'myPic' } }
+    });
 
-//     scope.$on('NavBarController', function() {
-//       assert.equal(element.find('.title').text().trim(), 'Portfolio Builder');
+    scope.$on('NavBarController', function() {
+      assert.equal(element.find('.title').text().trim(), 'Portfolio Builder');
 
-//       httpBackend.flush();
-//       assert.notEqual(element.find('.user-info .user').css('display'), 'none');
-//       assert.equal(element.find('.user-info .user img').attr('src'), 'myPic');
-//       done();
-//     });
-//   });
-// });
+      httpBackend.flush();
+      assert.notEqual(element.find('.user-info .user').css('display'), 'none');
+      assert.equal(element.find('.user-info .user img').attr('src'), 'myPic');
+      done();
+    });
+  });
+});
 
 describe('Project Details', function() {
   var injector;
@@ -44,7 +44,7 @@ describe('Project Details', function() {
   var httpBackend;
 
   beforeEach(function() {
-    injector = angular.injector(['myApp.components', 'ngMockE2E']);
+    injector = angular.injector(['myApp.components', 'ngMockE2E', 'myApp']);
     intercepts = {};
 
     injector.invoke(function($rootScope, $compile, $httpBackend, $ngRoute) {
