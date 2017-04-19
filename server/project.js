14ea4a2689a631ff48f6ac1b0279268849bebe89
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-04-19 12:39:27 -0500

    Refactor: update data

--- server/project.js
+++ server/project.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 module.exports = function(goose_for_db) {
   let projectSchema = {
     id: { type: String, required: [true, 'why no id?'] },
-    genre: { type: String, enum: ['full stack', 'front-end', 'back-end'], required: [true, 'why no genre?']},
+    genre: { type: String, enum: ['full stack', 'front-end', 'back-end', 'database'], required: [true, 'why no genre?']},
     title: { type: String, required: [true, 'why no title?']},
     year_start: { type: Number, required: [true, 'why no year_start?']},
     year_end: { type: Number },
