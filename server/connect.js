4e5a15de30c18bbbf6222ff391eac7ce523048ae
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-04-22 22:44:12 -0500

    Fix: address err on heroku logs

--- server/connect.js
+++ server/connect.js
@@ -1,7 +1,6 @@
 var mongodb = require('mongodb');
 var fs = require('fs');
 
-//var dburi = require('./dburi');
 var dburi = process.env.MONGODB_URI;
 
 module.exports = function(callback) {
--- server/models.js
+++ server/models.js
@@ -1,13 +1,12 @@
 var mongoose = require('mongoose');
 var _ = require('underscore');
-//var dburi = require('./dburi');
+
 var dburi = process.env.MONGODB_URI;
 
 module.exports = function(wagner) {
   var options = { promiseLibrary: require('bluebird') };
 
   mongoose.connect(dburi, options);
-  //If using local db: mongoose.connect('mongodb://localhost:27017/portfolio');
 
   wagner.factory('goose_for_db', function() {
     return mongoose;
@@ -22,8 +21,6 @@ module.exports = function(wagner) {
     });
   });
   wagner.factory('Project', require('./project'));
-
-  //wagner.factory('User', User);
 
   return models;
 };
