dd45de2a3553fb1ae50dbd251246fd39057d024b
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-04-26 10:28:21 -0500

    Test

--- server/api.js
+++ server/api.js
@@ -169,7 +169,11 @@ function handleMany(property, res, error, result) {
       status(status.INTERNAL_SERVER_ERROR).
       json({ error: error.toString() });
   }
-
+  if (!result) {
+    return res.
+      status(status.NOT_FOUND).
+      json({ error: 'Not found' });
+  }
   var json = {};
   json[property] = result;
   res.json(json);
@@ -181,6 +185,12 @@ function handleManyPromise(property, res, error, result) {
     return res.
       status(status.INTERNAL_SERVER_ERROR).
       json({ error: error.toString() });
+  }
+
+  if (!result) {
+    return res.
+      status(status.NOT_FOUND).
+      json({ error: 'Not found' });
   }
 
   var json = {};
