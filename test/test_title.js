161e745b039af171c0afc3dd88557e9a98a3cce5
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-04-29 12:20:51 -0500

    Test: make use of assert library of Node in test

--- test/test_title.js
+++ test/test_title.js
@@ -7,17 +7,17 @@ describe('Portfolio Page app', function() {
 
 	it('should wait for 3 seconds', function() {
 		browser.pause(3000);
+		console.log('I waited');
 	});
 
     it('should have a title', function() {
         var title = browser.getTitle();
-        // assert.equal(title, 'Portfolio Site');
-        console.log('title', title);
+        assert.equal(title, 'Portfolio Site');
     });
 
     it('should have a url', function() {
         var url = browser.getUrl();
-        // assert.equal(url, baseUrl);
-        console.log('url', url);
+        var containsUrl = url.includes('//portfolio/#/');
+        assert.ok(containsUrl);
     });
 });
