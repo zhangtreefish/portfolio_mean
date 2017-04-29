7aa667f71e001bfdf0d95f841fb07bac2e375af1
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-04-29 18:20:13 -0500

    Fix:path for local dev

--- server/dependencies.js
+++ server/dependencies.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 module.exports = function(wagner) {
   var Config_func = wagner.factory('Config', function() {
-    return JSON.parse(fs.readFileSync('./server/config.json').toString());
+    return JSON.parse(fs.readFileSync('./config.json').toString());
   });
 
   return {Config: Config_func};
