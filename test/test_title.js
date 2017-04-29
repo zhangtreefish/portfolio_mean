ebbdc20e63b1365b3b5dbd51f8e11cb9c054e92e
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-04-29 17:39:08 -0500

    Test: functional test on #pickTool

--- test/test_title.js
+++ test/test_title.js
@@ -1,8 +1,8 @@
-describe('Portfolio Page app', function() {
-	before(function(){
+before(function(){
         browser.url('/portfolio/');
 	})
 
+describe('Portfolio Page app', function() {
 	it('should wait for 3 seconds', function() {
 		browser.pause(3000);
 		console.log('I waited');
@@ -18,3 +18,18 @@ describe('Portfolio Page app', function() {
         expect(url).to.include('//portfolio/#/', 'url mismatch');
     });
 });
+
+describe('#pickTool', function() {
+	it('should have a height', function() {
+		var height = browser.getCssProperty('#pickTool', 'height');
+		expect(height.parsed.value).to.be.at.least(0, "#pickTool height not >= 0!");
+	});
+	//TODO:
+	// it('should go to whatever is picked', function() {
+	// 	browser.setValue('#pickTool', 'javascript');
+	// 	browser.click('#pickTool');
+	// 	var tools = browser.getValue('#main > div > div > tool-projects-two > div');
+ //        console.log(tools);
+ //        //expect(url).to.include('//portfolio/#/', 'url mismatch');
+	// })
+})
